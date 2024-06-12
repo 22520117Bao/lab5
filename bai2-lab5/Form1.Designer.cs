e3d4c0dbd4741793bfd3c58f3672272a991ce306
22520117Bao <162546070+22520117Bao@users.noreply.github.com>
2024-06-12 17:12:41 +0700

    Add files via upload

---
+++ bai2-lab5/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbtex = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.btlogin = new System.Windows.Forms.Button();
+            this.listViewEmails = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtrecent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbtex
+            // 
+            this.lbtex.AutoSize = true;
+            this.lbtex.Location = new System.Drawing.Point(4, 14);
+            this.lbtex.Name = "lbtex";
+            this.lbtex.Size = new System.Drawing.Size(35, 13);
+            this.lbtex.TabIndex = 0;
+            this.lbtex.Text = "Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total";
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(67, 7);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(371, 20);
+            this.txtemail.TabIndex = 4;
+            this.txtemail.Text = "kaigiabao1009@gmail.com";
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(67, 44);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(371, 20);
+            this.txtpass.TabIndex = 5;
+            this.txtpass.Text = "spet fooy vzqb ddes";
+            // 
+            // txttotal
+            // 
+            this.txttotal.Location = new System.Drawing.Point(63, 79);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(51, 20);
+            this.txttotal.TabIndex = 6;
+            // 
+            // btlogin
+            // 
+            this.btlogin.Location = new System.Drawing.Point(457, 7);
+            this.btlogin.Name = "btlogin";
+            this.btlogin.Size = new System.Drawing.Size(123, 57);
+            this.btlogin.TabIndex = 8;
+            this.btlogin.Text = "LOGIN";
+            this.btlogin.UseVisualStyleBackColor = true;
+            this.btlogin.Click += new System.EventHandler(this.btlogin_Click);
+            // 
+            // listViewEmails
+            // 
+            this.listViewEmails.HideSelection = false;
+            this.listViewEmails.Location = new System.Drawing.Point(12, 105);
+            this.listViewEmails.Name = "listViewEmails";
+            this.listViewEmails.Size = new System.Drawing.Size(587, 219);
+            this.listViewEmails.TabIndex = 9;
+            this.listViewEmails.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Recent";
+            // 
+            // txtrecent
+            // 
+            this.txtrecent.Location = new System.Drawing.Point(231, 79);
+            this.txtrecent.Name = "txtrecent";
+            this.txtrecent.Size = new System.Drawing.Size(59, 20);
+            this.txtrecent.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 336);
+            this.Controls.Add(this.txtrecent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listViewEmails);
+            this.Controls.Add(this.btlogin);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbtex);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbtex;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.Button btlogin;
+        private System.Windows.Forms.ListView listViewEmails;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtrecent;
+    }
+}
+
---
+++ bai2-lab5/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Forms;
+using MailKit.Net.Imap;
+using MailKit;
+
+namespace test1
+{
+    public partial class Form1 : Form
+    {
+        // imap: spet fooy vzqb ddes
+        //pop :ebyh qelc cvpt yllo
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listViewEmails.View = View.Details;
+            listViewEmails.Columns.Add("Subject", 200);
+            listViewEmails.Columns.Add("From", 200);
+            listViewEmails.Columns.Add("Date", 150);
+        }
+
+        private void btlogin_Click(object sender, EventArgs e)
+        {
+            string email = txtemail.Text.Trim();
+            string password = txtpass.Text.Trim();
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Nhap day du vao o trong", "Warning", MessageBoxButtons.OK);
+            }
+            else
+            {
+                EmailLoading(email, password);
+            }
+        }
+
+        private async void EmailLoading(string email, string password)
+        {
+            try
+            {
+                using (var client = new ImapClient())
+                {
+                    // Bỏ qua kiểm tra chứng chỉ
+                    client.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+                    await client.ConnectAsync("imap.gmail.com", 993, true);
+                    await client.AuthenticateAsync(email, password);
+                    var inbox = client.Inbox;
+                    await inbox.OpenAsync(FolderAccess.ReadOnly);
+                    int totalEmails = inbox.Count;
+                    txttotal.Text = totalEmails.ToString(); // Hiển thị tổng số email
+                    // Lấy số lượng email cần đọc từ textbox
+                    int emailCountToFetch = int.Parse(txtrecent.Text.Trim());
+                    // Kiểm tra nếu số lượng cần đọc lớn hơn tổng số email thì lấy tổng số email
+                    if (emailCountToFetch > totalEmails)
+                    {
+                        emailCountToFetch = totalEmails;
+                    }
+                    listViewEmails.Items.Clear();
+                    for (int i = totalEmails - emailCountToFetch; i < totalEmails; i++)
+                    {
+                        var message = await inbox.GetMessageAsync(i);
+                        var listViewItem = new ListViewItem(new[] {
+                            message.Subject,
+                            message.From.ToString(),
+                            message.Date.DateTime.ToString()
+                        });
+                        listViewEmails.Items.Add(listViewItem);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
