4452747193362441d74c7ce53402eff7f6bb3300
22520117Bao <162546070+22520117Bao@users.noreply.github.com>
2024-06-12 17:19:48 +0700

    Add files via upload

---
+++ Bai5/SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿--CREATE TABLE foodconfig
+--(
+--	idt INT PRIMARY KEY IDENTITY(1,1),
+--	email VARCHAR(MAX) NULL,
+--	username VARCHAR(MAX) NULL,
+--	passsword VARCHAR(MAX) NULL,
+--	tenmonan VARCHAR(MAX) NULL,
+--	gia INT NULL,
+--	mota VARCHAR(MAX) NULL,
+--	hinhanh VARCHAR(MAX) NULL,
+--	diachi VARCHAR(MAX) NULL
+
+--)
