1c6b58fcfae1bcfe18a9459881a25db1ec7ce9c6
22520117Bao <162546070+22520117Bao@users.noreply.github.com>
2024-06-12 17:08:03 +0700

    Add files via upload

---
+++ Bai1/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtfrom = new System.Windows.Forms.TextBox();
+            this.txtto = new System.Windows.Forms.TextBox();
+            this.txtsubject = new System.Windows.Forms.TextBox();
+            this.txtinfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btsend = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtfrom
+            // 
+            this.txtfrom.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfrom.Location = new System.Drawing.Point(154, 3);
+            this.txtfrom.Name = "txtfrom";
+            this.txtfrom.Size = new System.Drawing.Size(348, 26);
+            this.txtfrom.TabIndex = 0;
+            this.txtfrom.Text = "kaigiabao1009@gmail.com";
+            // 
+            // txtto
+            // 
+            this.txtto.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtto.Location = new System.Drawing.Point(154, 32);
+            this.txtto.Name = "txtto";
+            this.txtto.Size = new System.Drawing.Size(348, 26);
+            this.txtto.TabIndex = 1;
+            this.txtto.Text = "kaigiabao7944@gmail.com";
+            // 
+            // txtsubject
+            // 
+            this.txtsubject.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsubject.Location = new System.Drawing.Point(2, 97);
+            this.txtsubject.Name = "txtsubject";
+            this.txtsubject.Size = new System.Drawing.Size(689, 26);
+            this.txtsubject.TabIndex = 2;
+            // 
+            // txtinfo
+            // 
+            this.txtinfo.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtinfo.Location = new System.Drawing.Point(2, 147);
+            this.txtinfo.Multiline = true;
+            this.txtinfo.Name = "txtinfo";
+            this.txtinfo.Size = new System.Drawing.Size(689, 172);
+            this.txtinfo.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(96, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Subject";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Info";
+            // 
+            // btsend
+            // 
+            this.btsend.Location = new System.Drawing.Point(11, 6);
+            this.btsend.Name = "btsend";
+            this.btsend.Size = new System.Drawing.Size(75, 56);
+            this.btsend.TabIndex = 8;
+            this.btsend.Text = "Send";
+            this.btsend.UseVisualStyleBackColor = true;
+            this.btsend.Click += new System.EventHandler(this.btsend_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(73, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Password";
+            // 
+            // txtpass
+            // 
+            this.txtpass.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.Location = new System.Drawing.Point(154, 65);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(348, 26);
+            this.txtpass.TabIndex = 10;
+            this.txtpass.Text = "spet fooy vzqb ddes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 320);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btsend);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtinfo);
+            this.Controls.Add(this.txtsubject);
+            this.Controls.Add(this.txtto);
+            this.Controls.Add(this.txtfrom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtfrom;
+        private System.Windows.Forms.TextBox txtto;
+        private System.Windows.Forms.TextBox txtsubject;
+        private System.Windows.Forms.TextBox txtinfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btsend;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtpass;
+    }
+}
+
---
+++ Bai1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace Bai1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btsend_Click(object sender, EventArgs e)
+        {
+            string from, to, pass, content, sub;
+            from = txtfrom.Text.Trim();
+            to = txtto.Text.Trim();
+            pass = txtpass.Text.Trim();
+            content = txtinfo.Text.Trim();
+            sub = txtsubject.Text.Trim();
+
+            MailMessage smtp = new MailMessage();
+            smtp.To.Add(to);
+            smtp.From = new MailAddress(from);
+            smtp.Subject = sub;
+            smtp.Body = content;
+
+            System.Net.Mail.SmtpClient mail = new System.Net.Mail.SmtpClient("smtp.gmail.com");
+            mail.EnableSsl = true;
+            mail.Port = 587;
+            mail.DeliveryMethod = SmtpDeliveryMethod.Network;
+            mail.Credentials = new NetworkCredential(from, pass);
+            try
+            {
+                mail.Send(smtp);
+                MessageBox.Show("Send success");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"error");
+            }
+        }
+    }
+}
