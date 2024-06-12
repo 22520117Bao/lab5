1df9120a93a88676af048c7a2d747f60f45eabeb
22520117Bao <162546070+22520117Bao@users.noreply.github.com>
2024-06-12 17:14:01 +0700

    Add files via upload

---
+++ Bai3/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Bai3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtrecent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listViewEmails = new System.Windows.Forms.ListView();
+            this.btlogin = new System.Windows.Forms.Button();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbtex = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtrecent
+            // 
+            this.txtrecent.Location = new System.Drawing.Point(231, 81);
+            this.txtrecent.Name = "txtrecent";
+            this.txtrecent.Size = new System.Drawing.Size(59, 20);
+            this.txtrecent.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Recent";
+            // 
+            // listViewEmails
+            // 
+            this.listViewEmails.HideSelection = false;
+            this.listViewEmails.Location = new System.Drawing.Point(12, 107);
+            this.listViewEmails.Name = "listViewEmails";
+            this.listViewEmails.Size = new System.Drawing.Size(587, 202);
+            this.listViewEmails.TabIndex = 19;
+            this.listViewEmails.UseCompatibleStateImageBehavior = false;
+            // 
+            // btlogin
+            // 
+            this.btlogin.Location = new System.Drawing.Point(444, 9);
+            this.btlogin.Name = "btlogin";
+            this.btlogin.Size = new System.Drawing.Size(123, 57);
+            this.btlogin.TabIndex = 18;
+            this.btlogin.Text = "LOGIN";
+            this.btlogin.UseVisualStyleBackColor = true;
+            this.btlogin.Click += new System.EventHandler(this.btlogin_Click);
+            // 
+            // txttotal
+            // 
+            this.txttotal.Location = new System.Drawing.Point(63, 81);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(51, 20);
+            this.txttotal.TabIndex = 17;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(67, 46);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(371, 20);
+            this.txtpass.TabIndex = 16;
+            this.txtpass.Text = "ebyh qelc cvpt yllo";
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(67, 9);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(371, 20);
+            this.txtemail.TabIndex = 15;
+            this.txtemail.Text = "kaigiabao1009@gmail.com";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Total";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Password:";
+            // 
+            // lbtex
+            // 
+            this.lbtex.AutoSize = true;
+            this.lbtex.Location = new System.Drawing.Point(4, 16);
+            this.lbtex.Name = "lbtex";
+            this.lbtex.Size = new System.Drawing.Size(35, 13);
+            this.lbtex.TabIndex = 12;
+            this.lbtex.Text = "Email:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 329);
+            this.Controls.Add(this.txtrecent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listViewEmails);
+            this.Controls.Add(this.btlogin);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbtex);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtrecent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listViewEmails;
+        private System.Windows.Forms.Button btlogin;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbtex;
+    }
+}
+
---
+++ Bai3/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using MailKit.Net.Pop3;
+using MimeKit;
+
+namespace Bai3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btlogin_Click(object sender, EventArgs e)
+        {
+            string email = txtemail.Text.Trim();
+            string password = txtpass.Text.Trim();
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Nhap day du vao o trong", "Warning", MessageBoxButtons.OK);
+            }
+            else
+            {
+                EmailLoading(email, password);
+            }
+        }
+        private async void EmailLoading(string email, string password)
+        {            
+                try
+                {
+                    using (var client = new Pop3Client())
+                    {
+                        // Bỏ qua kiểm tra chứng chỉ
+                        client.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+                        await client.ConnectAsync("pop.gmail.com", 995, true);
+                        await client.AuthenticateAsync(email, password);
+                    int totalEmails = client.Count;
+                    txttotal.Text = totalEmails.ToString(); // Hiển thị tổng số email
+                    // Lấy số lượng email cần đọc từ textbox
+                    int emailCountToFetch = int.Parse(txtrecent.Text.Trim());
+                    // Kiểm tra nếu số lượng cần đọc lớn hơn tổng số email thì lấy tổng số email
+                    if (emailCountToFetch > totalEmails)
+                    {
+                        emailCountToFetch = totalEmails;
+                    }
+                    List<MimeMessage> recentEmails = new List<MimeMessage>();
+                    listViewEmails.Items.Clear();
+                        for (int i = totalEmails - emailCountToFetch; i < totalEmails ; i++)
+                        {
+                            var message = await client.GetMessageAsync(i);
+                            recentEmails.Add(message);
+                        }
+                    recentEmails.Reverse();
+                    foreach (var message in recentEmails)
+                    {
+                        var listViewItem = new ListViewItem(new[] {
+                            message.Subject,
+                            message.From.ToString(),
+                            message.Date.DateTime.ToString()
+                        });
+
+                        listViewEmails.Items.Add(listViewItem);
+                    }
+                    await client.DisconnectAsync(true);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listViewEmails.View = View.Details;
+            listViewEmails.Columns.Add("Subject", 200);
+            listViewEmails.Columns.Add("From", 200);
+            listViewEmails.Columns.Add("Date", 150);            
+        }
+    }
+}
